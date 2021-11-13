b60122ed385ef35ea59392334f07bf52a90428c7
bulidiriba <bulidiriba@gmail.com>
2021-11-13 18:11:39 +0300

    add the scrapping of each and every actors and writers

--- movie-data-scrapper.py
+++ movie-data-scrapper.py
@@ -12,7 +12,10 @@ from urllib.request import urlopen
 movie_id = sys.argv[1]
 print("Id: ", sys.argv[1])
 
-url = "https://elcinema.com/work/"+movie_id
+movie_url_base = "https://elcinema.com/work/"
+person_url_base = "https://elcinema.com/person/"
+
+url = movie_url_base+movie_id
 html = urlopen(url)
 bsObj = BeautifulSoup(html, "html.parser")
 
@@ -72,24 +75,103 @@ print("Description: ", description)
 
 # get the director
 director = row_2[11].text
+#print(row_2[11])
 print("Director: ", director.strip().split("\n")[2])
 
 # get the Writer
 writers = []
+writers_url = {}
 for tag in row_2[13]:
     for t in tag:
         if (type(t) is str):
             writers = writers
         else:
-            if t.name == "a":writers.append(t.text)
+            if t.name == "a":
+                writers.append(t.text)
+                print("writers link: ", t['href'])
+                writers_id = t['href'].split("/")[2]
+                writers_url[t.text] = writers_id
+writers.pop(-1) # remove the last element which is "(more)" text
+writers_url.pop('(المزيد)') # remove the last element which is "(more)" element
 print("wirters: ", writers)
+print("Writers url: ", writers_url)
 
 # get the actor
 actors = []
+actors_url = {}
 for tag in row_2[15]:
     for t in tag:
         if (type(t) is str):
             actors = actors
         else:
-            if t.name == "a":actors.append(t.text)
-print("actors: ", actors)
+            if t.name == "a":
+                actors.append(t.text)
+                actor_id = t['href'].split("/")[2]
+                actors_url[t.text] = actor_id
+actors.pop(-1) # remove the last element which is "(more)" text
+actors_url.pop('(المزيد)') # remove the last element which is "(more)" element
+print("Actors: ", actors)
+print("Actors URL: ", actors_url)
+
+def get_person_data(person_name, person_id):
+    #person_id = "1112763"
+    person_data = {"Name":person_name, "image":"", "description":"", "nationality":"", "date_of_birth":"","place_of_birth":""}
+    url = person_url_base+person_id
+    html = urlopen(url)
+    bsObj = BeautifulSoup(html, "html.parser")
+
+    # get the intor box div
+    introBoxDv = bsObj.find('div', {'class':'intro-box'})
+    intro_array = [dv for dv in introBoxDv]
+    row_array = [dv for dv in intro_array[1]]
+    row_1 = [tag for tag in row_array[1]]
+    row_2 = [tag for tag in row_array[3]]
+
+    # get the poster
+    imgtag = [tag for tag in row_1[1]]
+    person_image_url = imgtag[0]['src']
+ 
+    # get the description
+    description = row_2[3].text
+
+    # get the nationality
+    # check weather the description for person exist or not, if exist extract it
+    try:
+        info = row_2[5].find_all('ul', {'class':'list-separator list-title'})
+        nationality = info[0].find('a').text
+        date_of_birth = info[1].find_all('a')[0].text + " " + info[1].find_all('a')[1].text
+        place_of_birth = info[2].find_all('li')[1].text
+    except:
+        nationality = ""
+        date_of_birth = ""
+        place_of_birth = ""
+
+    # update person image, description, nationality, date of birth and place of birth with the scrapped data
+    person_data["description"] = description
+    person_data["image"] = person_image_url
+    person_data["nationality"] = nationality
+    person_data["date_of_birth"] = date_of_birth
+    person_data["place_of_birth"] = place_of_birth
+
+    return person_data
+
+print("----------WRITERS DATA------")
+# get the writers data
+for writer in writers_url:
+    person_id = writers_url[writer]
+    person_name = writer
+    person_data = get_person_data(person_name, person_id)
+    print(person_data)
+
+print("---------ACTORS DATA--------")
+# get the actors data
+for actor in actors_url:
+    person_id = actors_url[actor]
+    person_name = actor
+    person_data = get_person_data(person_name, person_id)
+    print(person_data)
+
+
+# Task TBD
+# 1. Scrapping the information of each actors, writers and directors
+
